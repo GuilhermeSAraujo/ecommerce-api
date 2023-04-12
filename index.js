bc486648dcbcf72c1694ae1b76c9e4788585e564
Guilherme Souza Araújo <guilherme.souza@dtidigital.com.br>
2023-04-12 20:39:08 -0300

    Adding auth-middleware

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 import express from "express";
+import cors from 'cors';
+const authMiddleware = require("./auth-middleware");
+
 const PORT = process.env.PORT || 3030;
 
 var app = express();
+app.use(cors());
+
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
 
-app.get("/products", (req, res, next) => {
-	res.json(["toys"]);
+app.use("/", authMiddleware);
+
+app.get("/products", (req, res) => {
 	console.log("Calling /products");
+	res.json(["toys"]);
 });
 
-app.get("/user", (req, res, next) => {
+app.get("/user", (req, res) => {
 	console.log("Calling /user");
 	res.json({ id: 1, name: "Guilherme" });
 });
